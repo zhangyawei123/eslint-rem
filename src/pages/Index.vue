84346598158667aca631a11d6811130c017d1c64
zyw <zyw568579931@163.com>
2019-04-25 10:09:02 +0800

    eslint自动修复和移动端rem

---
+++ src/pages/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>this is index page</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Index.vue"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/Other.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>this is other page</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Other.vue"
+  }
+</script>
+
+<style scoped>
+
+</style>
